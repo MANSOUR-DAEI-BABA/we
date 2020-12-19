439bd43ec83515d75804cb0f9b765170ac0dd94e
MANSOUR-DAEI-BABA <74072296+MANSOUR-DAEI-BABA@users.noreply.github.com>
2020-12-19 07:24:47 -0800

    سایت من
    سایت من

---
+++ chat room (5)/ab.js
@@ -0,0 +1,6 @@
+(function (f) { f(P._namespace('gwiAutoInstVisible')); }(function (P) { if (window.GWI) { GWI.Card.autoInstVisible('oFetLCWhQeOWJ5l2Gvnhcw'); } }));
+P.when('mix:fluid-fat-image-link').execute(function (cardModule) { cardModule.init('oFetLCWhQeOWJ5l2Gvnhcw');
+ if (window.GWI) { window.GWI.Card.autoInstActive('oFetLCWhQeOWJ5l2Gvnhcw'); } });
+ (function (f) { f(P._namespace('gwiAutoInstVisible')); }(function (P) { if (window.GWI) { GWI.Card.autoInstVisible('8gh5nwHFokrAQsguwWddvA'); } }));
+ P.when('mix:fluid-fat-image-link').execute(function (cardModule) { cardModule.init('8gh5nwHFokrAQsguwWddvA'); if (window.GWI) { window.GWI.Card.autoInstActive('8gh5nwHFokrAQsguwWddvA'); } });
+          
